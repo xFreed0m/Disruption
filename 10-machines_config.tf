bb6f3c8e65de6169b758a0745bfc15347fe7891c
xFreed0m <roei.sm@gmail.com>
2020-02-11 16:43:39 +0200

    version syntax fixed
    TF version 12 syntax fixes

--- 10-machines_config.tf
+++ 10-machines_config.tf
@@ -9,30 +9,33 @@ locals {
   install_dns_command  = "Install-WindowsFeature DNS -IncludeManagementTools"
   configure_ad_command = "Install-ADDSForest -CreateDnsDelegation:$false -DomainMode Win2012R2 -DomainName ${var.domain_name} -DomainNetbiosName ${var.netbios_domain_name} -ForestMode Win2012R2 -InstallDns:$true -SafeModeAdministratorPassword $password -Force:$true"
   shutdown_command     = "shutdown -r -t 10"
-# Exit code hack is needed to prevent the terraform deployer from thinking the command failed
-  exit_code_hack       = "exit 0"
-  win7_set_dns         = "netsh interface ip set dns 'Local Area Connection' static ${var.int_dns_address}"
-  powershell_command   = "${local.ps_exec_policy}; ${local.choco_install}; ${local.choco_pks}; ${local.import_command}; ${local.password_command}; ${local.install_ad_command}; ${local.install_dns_command}; ${local.configure_ad_command}; ${local.shutdown_command}; ${local.exit_code_hack}"
-  fileserver_install   = "Install-WindowsFeature -Name FS-FileServer -IncludeAllSubFeature -IncludeManagementTools"
-  webserver_install    = "Install-WindowsFeature -name Web-Server -IncludeManagementTools"
-  mkdir_temp           = "mkdir C:/Temp" 
-  fileserver_share     = "New-SmbShare -Name fileshare -Path C:/Temp -FullAccess Everyone"
-  dc2user_command         = "$dc2user = ${var.username}" 
+
+  # Exit code hack is needed to prevent the terraform deployer from thinking the command failed
+  exit_code_hack          = "exit 0"
+  win7_set_dns            = "netsh interface ip set dns 'Local Area Connection' static ${var.int_dns_address}"
+  powershell_command      = "${local.ps_exec_policy}; ${local.choco_install}; ${local.choco_pks}; ${local.import_command}; ${local.password_command}; ${local.install_ad_command}; ${local.install_dns_command}; ${local.configure_ad_command}; ${local.shutdown_command}; ${local.exit_code_hack}"
+  fileserver_install      = "Install-WindowsFeature -Name FS-FileServer -IncludeAllSubFeature -IncludeManagementTools"
+  webserver_install       = "Install-WindowsFeature -name Web-Server -IncludeManagementTools"
+  mkdir_temp              = "mkdir C:/Temp"
+  fileserver_share        = "New-SmbShare -Name fileshare -Path C:/Temp -FullAccess Everyone"
+  dc2user_command         = "$dc2user = ${var.username}"
   dc2creds_command        = "$mycreds = New-Object System.Management.Automation.PSCredential -ArgumentList $dc2user, $password"
   dc2configure_ad_command = "Install-ADDSDomainController -Credential $mycreds -CreateDnsDelegation:$false -DomainName ${var.domain_name} -InstallDns:$true -SafeModeAdministratorPassword $password -Force:$true"
   dc2shutdown_command     = "shutdown -r -t 10"
   ps_exec_policy          = "Set-ExecutionPolicy Bypass -Force"
   choco_install           = "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))"
   choco_pks               = "powershell.exe -Command choco install ${var.chrome} ${var.notepad} ${var.s7z} ${var.git} ${var.sysint} ${var.py3} ${var.py2} -y"
+
   # Clients sometimes needs to refresh the DNS server address or they won't be able to find the DC ¯\_(ツ)_/¯
-  set_dns                 = "Set-DnsClientServerAddress -InterfaceAlias 'Ethernet' -ServerAddresses ('${var.int_dns_address}', '1.1.1.1')"
-  dc2powershell_command   = "${local.ps_exec_policy}; ${local.set_dns}; ${local.choco_install}; ${local.choco_pks}; ${local.import_command}; ${local.dc2user_command}; ${local.password_command}; ${local.dc2creds_command}; ${local.install_ad_command}; ${local.dc2configure_ad_command}; ${local.dc2shutdown_command}; ${local.exit_code_hack}"
+  set_dns               = "Set-DnsClientServerAddress -InterfaceAlias 'Ethernet' -ServerAddresses ('${var.int_dns_address}', '1.1.1.1')"
+  dc2powershell_command = "${local.ps_exec_policy}; ${local.set_dns}; ${local.choco_install}; ${local.choco_pks}; ${local.import_command}; ${local.dc2user_command}; ${local.password_command}; ${local.dc2creds_command}; ${local.install_ad_command}; ${local.dc2configure_ad_command}; ${local.dc2shutdown_command}; ${local.exit_code_hack}"
 }
+
 resource "azurerm_virtual_machine_extension" "dc1primary_commands" {
   name                 = "dc1primary_commands"
-  location             = "${var.location}"
-  resource_group_name  = "${var.rg}"
-  virtual_machine_name = "${azurerm_virtual_machine.dc1primary.name}"
+  location             = var.location
+  resource_group_name  = var.rg
+  virtual_machine_name = azurerm_virtual_machine.dc1primary.name
   publisher            = "Microsoft.Compute"
   type                 = "CustomScriptExtension"
   type_handler_version = "1.9"
@@ -42,7 +45,9 @@ resource "azurerm_virtual_machine_extension" "dc1primary_commands" {
         "commandToExecute": "powershell.exe -Command \"${local.powershell_command}\" "
     }
 SETTINGS
-  depends_on = ["azurerm_virtual_machine.dc1primary"]
+
+
+  depends_on = [azurerm_virtual_machine.dc1primary]
 }
 
 ####################################
@@ -51,9 +56,9 @@ SETTINGS
 ####################################
 resource "azurerm_virtual_machine_extension" "dc2_commands" {
   name                 = "dc2_commands"
-  location             = "${var.location}"
-  resource_group_name  = "${var.rg}"
-  virtual_machine_name = "${azurerm_virtual_machine.dc2sub.name}"
+  location             = var.location
+  resource_group_name  = var.rg
+  virtual_machine_name = azurerm_virtual_machine.dc2sub.name
   publisher            = "Microsoft.Compute"
   type                 = "CustomScriptExtension"
   type_handler_version = "1.9"
@@ -63,19 +68,20 @@ resource "azurerm_virtual_machine_extension" "dc2_commands" {
         "commandToExecute": "powershell.exe -Command \"${local.dc2powershell_command}\" "
     }
 SETTINGS
-  depends_on = ["azurerm_virtual_machine_extension.join-domain_dc2"]
+
+
+  depends_on = [azurerm_virtual_machine_extension.join-domain_dc2]
 }
 
 resource "azurerm_virtual_machine_extension" "join-domain_dc2" {
   name                 = "join-domain_dc2"
-  location             = "${var.location}"
-  resource_group_name  = "${var.rg}"
-  virtual_machine_name = "${azurerm_virtual_machine.dc2sub.name}"
+  location             = var.location
+  resource_group_name  = var.rg
+  virtual_machine_name = azurerm_virtual_machine.dc2sub.name
   publisher            = "Microsoft.Compute"
   type                 = "JsonADDomainExtension"
   type_handler_version = "1.3"
 
-  
   # NOTE: the `OUPath` field is intentionally blank, to put it in the Computers OU
   settings = <<SETTINGS
     {
@@ -87,12 +93,15 @@ resource "azurerm_virtual_machine_extension" "join-domain_dc2" {
     }
 SETTINGS
 
+
   protected_settings = <<SETTINGS
     {
         "Password": "${var.password}"
     }
 SETTINGS
-  depends_on = ["azurerm_virtual_machine_extension.dc1primary_commands"]
+
+
+  depends_on = [azurerm_virtual_machine_extension.dc1primary_commands]
 }
 
 ###################################
@@ -101,9 +110,9 @@ SETTINGS
 ###################################
 resource "azurerm_virtual_machine_extension" "join-domain_fileserver" {
   name                 = "join-domain_domain_fileserver"
-  location             = "${var.location}"
-  resource_group_name  = "${var.rg}"
-  virtual_machine_name = "${azurerm_virtual_machine.fileserver.name}"
+  location             = var.location
+  resource_group_name  = var.rg
+  virtual_machine_name = azurerm_virtual_machine.fileserver.name
   publisher            = "Microsoft.Compute"
   type                 = "JsonADDomainExtension"
   type_handler_version = "1.3"
@@ -119,19 +128,25 @@ resource "azurerm_virtual_machine_extension" "join-domain_fileserver" {
     }
 SETTINGS
 
+
   protected_settings = <<SETTINGS
     {
         "Password": "${var.password}"
     }
 SETTINGS
-  depends_on = ["azurerm_virtual_machine_extension.dc1primary_commands", "azurerm_virtual_machine_extension.fileserver_commands"]
+
+
+  depends_on = [
+    azurerm_virtual_machine_extension.dc1primary_commands,
+    azurerm_virtual_machine_extension.fileserver_commands,
+  ]
 }
 
 resource "azurerm_virtual_machine_extension" "fileserver_commands" {
   name                 = "fileserver_commands"
-  location             = "${var.location}"
-  resource_group_name  = "${var.rg}"
-  virtual_machine_name = "${azurerm_virtual_machine.fileserver.name}"
+  location             = var.location
+  resource_group_name  = var.rg
+  virtual_machine_name = azurerm_virtual_machine.fileserver.name
   publisher            = "Microsoft.Compute"
   type                 = "CustomScriptExtension"
   type_handler_version = "1.9"
@@ -142,7 +157,8 @@ resource "azurerm_virtual_machine_extension" "fileserver_commands" {
     }
 SETTINGS
 
-  depends_on = ["azurerm_virtual_machine.fileserver"]
+
+  depends_on = [azurerm_virtual_machine.fileserver]
 }
 
 ####################################
@@ -151,9 +167,9 @@ SETTINGS
 ####################################
 resource "azurerm_virtual_machine_extension" "join-domain_client10" {
   name                 = "join-domain_client10"
-  location             = "${var.location}"
-  resource_group_name  = "${var.rg}"
-  virtual_machine_name = "${azurerm_virtual_machine.client10.name}"
+  location             = var.location
+  resource_group_name  = var.rg
+  virtual_machine_name = azurerm_virtual_machine.client10.name
   publisher            = "Microsoft.Compute"
   type                 = "JsonADDomainExtension"
   type_handler_version = "1.3"
@@ -169,19 +185,25 @@ resource "azurerm_virtual_machine_extension" "join-domain_client10" {
     }
 SETTINGS
 
+
   protected_settings = <<SETTINGS
     {
         "Password": "${var.password}"
     }
 SETTINGS
-  depends_on = ["azurerm_virtual_machine_extension.dc1primary_commands", "azurerm_virtual_machine_extension.client10_commands"]
+
+
+  depends_on = [
+    azurerm_virtual_machine_extension.dc1primary_commands,
+    azurerm_virtual_machine_extension.client10_commands,
+  ]
 }
 
 resource "azurerm_virtual_machine_extension" "client10_commands" {
   name                 = "client10_commands"
-  location             = "${var.location}"
-  resource_group_name  = "${var.rg}"
-  virtual_machine_name = "${azurerm_virtual_machine.client10.name}"
+  location             = var.location
+  resource_group_name  = var.rg
+  virtual_machine_name = azurerm_virtual_machine.client10.name
   publisher            = "Microsoft.Compute"
   type                 = "CustomScriptExtension"
   type_handler_version = "1.9"
@@ -192,7 +214,8 @@ resource "azurerm_virtual_machine_extension" "client10_commands" {
     }
 SETTINGS
 
-  depends_on = ["azurerm_virtual_machine.client10"]
+
+  depends_on = [azurerm_virtual_machine.client10]
 }
 
 ####################################
@@ -201,9 +224,9 @@ SETTINGS
 ####################################
 resource "azurerm_virtual_machine_extension" "join-domain_client7" {
   name                 = "join-domain_client7"
-  location             = "${var.location}"
-  resource_group_name  = "${var.rg}"
-  virtual_machine_name = "${azurerm_virtual_machine.client7.name}"
+  location             = var.location
+  resource_group_name  = var.rg
+  virtual_machine_name = azurerm_virtual_machine.client7.name
   publisher            = "Microsoft.Compute"
   type                 = "JsonADDomainExtension"
   type_handler_version = "1.3"
@@ -219,19 +242,22 @@ resource "azurerm_virtual_machine_extension" "join-domain_client7" {
     }
 SETTINGS
 
+
   protected_settings = <<SETTINGS
     {
         "Password": "${var.password}"
     }
 SETTINGS
-  depends_on = ["azurerm_virtual_machine_extension.dc1primary_commands"]
+
+
+  depends_on = [azurerm_virtual_machine_extension.dc1primary_commands]
 }
 
 resource "azurerm_virtual_machine_extension" "client7_commands" {
   name                 = "client7_commands"
-  location             = "${var.location}"
-  resource_group_name  = "${var.rg}"
-  virtual_machine_name = "${azurerm_virtual_machine.client7.name}"
+  location             = var.location
+  resource_group_name  = var.rg
+  virtual_machine_name = azurerm_virtual_machine.client7.name
   publisher            = "Microsoft.Compute"
   type                 = "CustomScriptExtension"
   type_handler_version = "1.9"
@@ -241,5 +267,8 @@ resource "azurerm_virtual_machine_extension" "client7_commands" {
         "commandToExecute": "powershell.exe -Command \"${local.ps_exec_policy}; ${local.win7_set_dns}; ${local.choco_install}; ${local.choco_pks}; ${local.shutdown_command}; ${local.exit_code_hack}\" "
     }
 SETTINGS
-  depends_on = ["azurerm_virtual_machine.client7"]
+
+
+  depends_on = [azurerm_virtual_machine.client7]
 }
+
--- 9-kali.tf
+++ 9-kali.tf
@@ -11,79 +11,80 @@
 
 # External NIC to access Kali from the outside
 resource "azurerm_network_interface" "kali_externalnic" {
-    name                = "kali_extnic"
-    location            = "${var.location}"
-    resource_group_name = "${var.rg}"
-    network_security_group_id = "${azurerm_network_security_group.secgroup.id}"
+  name                      = "kali_extnic"
+  location                  = var.location
+  resource_group_name       = var.rg
+  network_security_group_id = azurerm_network_security_group.secgroup.id
 
-    ip_configuration {
-        primary                       = true
-        name                          = "kali_externalnic"
-        subnet_id                     = "${azurerm_subnet.subnet.id}"
-        private_ip_address_allocation = "Dynamic"
-        public_ip_address_id          = "${azurerm_public_ip.kalipublicip.id}"
-    }
+  ip_configuration {
+    primary                       = true
+    name                          = "kali_externalnic"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.kalipublicip.id
+  }
 }
 
 # Creating a NIC for internal network on Kali
 resource "azurerm_network_interface" "kali_internalnic" {
-    name                = "kali_intnic"
-    location            = "${var.location}"
-    resource_group_name = "${var.rg}"
-    ip_configuration {
-        name                          = "kali_internal"
-        subnet_id                     = "${azurerm_subnet.subnet.id}"
-        private_ip_address_allocation = "Dynamic"
-    }
+  name                = "kali_intnic"
+  location            = var.location
+  resource_group_name = var.rg
+  ip_configuration {
+    name                          = "kali_internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
 }
 
 #Creating kali-linux VM
 resource "azurerm_virtual_machine" "kali" {
-    name                         = "kali"
-    resource_group_name          = "${var.rg}"
-    location                     = "${var.location}"
-    network_interface_ids        = ["${azurerm_network_interface.kali_externalnic.id}", "${azurerm_network_interface.kali_internalnic.id}"]
-    vm_size                      = "Standard_D1_v2"
-    primary_network_interface_id = "${azurerm_network_interface.kali_externalnic.id}"
-    delete_os_disk_on_termination = true
-    delete_data_disks_on_termination = true
+  name                             = "kali"
+  resource_group_name              = var.rg
+  location                         = var.location
+  network_interface_ids            = [azurerm_network_interface.kali_externalnic.id, azurerm_network_interface.kali_internalnic.id]
+  vm_size                          = "Standard_D1_v2"
+  primary_network_interface_id     = azurerm_network_interface.kali_externalnic.id
+  delete_os_disk_on_termination    = true
+  delete_data_disks_on_termination = true
 
-    storage_image_reference {
-        publisher = "kali-linux"
-        offer = "kali-linux"
-        sku = "kali"
-        version = "latest"
-    }
+  storage_image_reference {
+    publisher = "kali-linux"
+    offer     = "kali-linux"
+    sku       = "kali"
+    version   = "latest"
+  }
 
-    storage_os_disk {
-        name    = "kalidisk"
-        caching = "ReadWrite"
-        create_option = "FromImage"
-    }
+  storage_os_disk {
+    name          = "kalidisk"
+    caching       = "ReadWrite"
+    create_option = "FromImage"
+  }
 
-    os_profile {
-        computer_name  = "kali"
-        admin_username = "${var.username}"
-        admin_password = "${var.password}"
-    }
+  os_profile {
+    computer_name  = "kali"
+    admin_username = var.username
+    admin_password = var.password
+  }
 
-    os_profile_linux_config {
-        disable_password_authentication = true
-        ssh_keys {
-            path     = "/home/landlord/.ssh/authorized_keys"
-            key_data = "${var.pub_key}"
-        }
+  os_profile_linux_config {
+    disable_password_authentication = true
+    ssh_keys {
+      path     = "/home/landlord/.ssh/authorized_keys"
+      key_data = var.pub_key
     }
+  }
 
-    # Mandatory section for Marketplace VMs
-    plan {
-        name = "kali"
-        publisher = "kali-linux"
-        product = "kali-linux"
-    }
+  # Mandatory section for Marketplace VMs
+  plan {
+    name      = "kali"
+    publisher = "kali-linux"
+    product   = "kali-linux"
+  }
 }
 
 # Kali update && upgrade 
+<<<<<<< Updated upstream
 # resource "azurerm_virtual_machine_extension" "kali_commands" {
 #   name                 = "kali_commands"
 #   location             = "${var.location}"
@@ -92,6 +93,16 @@ resource "azurerm_virtual_machine" "kali" {
 #   publisher            = "Microsoft.Azure.Extensions"
 #   type                 = "CustomScript"
 #   type_handler_version = "2.0"
+=======
+resource "azurerm_virtual_machine_extension" "kali_commands" {
+  name                 = "kali_commands"
+  location             = var.location
+  resource_group_name  = var.rg
+  virtual_machine_name = azurerm_virtual_machine.kali.name
+  publisher            = "Microsoft.Azure.Extensions"
+  type                 = "CustomScript"
+  type_handler_version = "2.0"
+>>>>>>> Stashed changes
 
 #   settings = <<SETTINGS
 #     {
@@ -99,5 +110,12 @@ resource "azurerm_virtual_machine" "kali" {
 #     }
 # SETTINGS
 
+<<<<<<< Updated upstream
 #   depends_on = ["azurerm_virtual_machine.kali"]
 # }
+=======
+
+  depends_on = [azurerm_virtual_machine.kali]
+}
+
+>>>>>>> Stashed changes
