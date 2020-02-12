86684c630a7811a18115a7acb5f3131293bac38d
xFreed0m <roei.sm@gmail.com>
2020-02-12 17:34:35 +0200

    0.12 upgrade

--- 3-secgroup_storage.tf
+++ 3-secgroup_storage.tf
@@ -1,47 +1,9 @@
 # Security group to enable RDP+SSH+WEB access to the public IPs
 data "external" "whatismyip" {
- program = ["${path.module}/whatismyip.sh"]
+  program = ["${path.module}/whatismyip.sh"]
 }
 
 resource "azurerm_network_security_group" "secgroup" {
-    name                = "myNetworkSecurityGroup"
-    location            = "${var.location}"
-    resource_group_name = "${var.rg}"
-
-    security_rule {
-        name                       = "RDP"
-        priority                   = 1001
-        direction                  = "Inbound"
-        access                     = "Allow"
-        protocol                   = "Tcp"
-        source_port_range          = "*"
-        destination_port_range     = "3389"
-        source_address_prefix      = "${data.external.whatismyip.result["internet_ip"]}/32"
-        destination_address_prefix = "*"
-    }
-    security_rule {
-        name                       = "SSH"
-        priority                   = 1002
-        direction                  = "Inbound"
-        access                     = "Allow"
-        protocol                   = "Tcp"
-        source_port_range          = "*"
-        destination_port_range     = "22"
-        source_address_prefix      = "${data.external.whatismyip.result["internet_ip"]}/32"
-        destination_address_prefix = "*"
-    }
-    security_rule {
-        name                       = "WEB"
-        priority                   = 1003
-        direction                  = "Inbound"
-        access                     = "Allow"
-        protocol                   = "Tcp"
-        source_port_range          = "*"
-        destination_port_range     = "80"
-        source_address_prefix      = "${data.external.whatismyip.result["internet_ip"]}/32"
-        destination_address_prefix = "*"
-    }
-
   name                = "myNetworkSecurityGroup"
   location            = var.location
   resource_group_name = var.rg
@@ -79,46 +41,24 @@ resource "azurerm_network_security_group" "secgroup" {
     source_address_prefix      = "${data.external.whatismyip.result["internet_ip"]}/32"
     destination_address_prefix = "*"
   }
-  security_rule {
-    name                       = "Caldera_admin"
-    priority                   = 1004
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "8888"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-  security_rule {
-    name                       = "Caldera_C2"
-    priority                   = 1005
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "5678"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
 }
 
 # Generate random text for a unique storage account name
 resource "random_id" "randomId" {
-    keepers = {
-        # Generate a new ID only when a new resource group is defined
-        resource_group = "${"${var.rg}"}"
-    }
+  keepers = {
+    # Generate a new ID only when a new resource group is defined
+    resource_group = var.rg
+  }
 
-    byte_length = 8
+  byte_length = 8
 }
 
 resource "azurerm_storage_account" "storageacct" {
-    name                     = "storage${random_id.randomId.hex}"
-    resource_group_name      = "${var.rg}"
-    location                 = "${var.location}"
-    account_tier             = "Standard"
-    account_replication_type = "LRS"
+  name                     = "storage${random_id.randomId.hex}"
+  resource_group_name      = var.rg
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
 
 resource "azurerm_storage_container" "storagecontainer" {
@@ -127,7 +67,3 @@ resource "azurerm_storage_container" "storagecontainer" {
   container_access_type = "private"
 }
 
-    name                  = "storagecontainer"
-    storage_account_name  = "${azurerm_storage_account.storageacct.name}"
-    container_access_type = "private"
-}
--- 8-client7.tf
+++ 8-client7.tf
@@ -4,47 +4,48 @@
 
 # Creating a NIC for internal network on Client7
 resource "azurerm_network_interface" "client7_internalnic" {
-    name                = "client7_intnic"
-    location            = "${var.location}"
-    resource_group_name = "${var.rg}"
-    ip_configuration {
-        name                          = "client7_internal"
-        subnet_id                     = "${azurerm_subnet.subnet.id}"
-        private_ip_address_allocation = "Dynamic"
-    }
+  name                = "client7_intnic"
+  location            = var.location
+  resource_group_name = var.rg
+  ip_configuration {
+    name                          = "client7_internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
 }
 
 #Creating client7 VM
 resource "azurerm_virtual_machine" "client7" {
-    name                         = "client7"
-    resource_group_name          = "${var.rg}"
-    location                     = "${var.location}"
-    network_interface_ids        = ["${azurerm_network_interface.client7_internalnic.id}"]
-    vm_size                      = "Standard_D1_v2"
-    primary_network_interface_id = "${azurerm_network_interface.client7_internalnic.id}"
-    delete_os_disk_on_termination = true
-    delete_data_disks_on_termination = true
+  name                             = "client7"
+  resource_group_name              = var.rg
+  location                         = var.location
+  network_interface_ids            = [azurerm_network_interface.client7_internalnic.id]
+  vm_size                          = "Standard_D1_v2"
+  primary_network_interface_id     = azurerm_network_interface.client7_internalnic.id
+  delete_os_disk_on_termination    = true
+  delete_data_disks_on_termination = true
 
-    storage_image_reference {
-        publisher = "MicrosoftWindowsDesktop"
-        offer = "Windows-7"
-        sku = "win7-enterprise"
-        version = "latest"
-    }
+  storage_image_reference {
+    publisher = "MicrosoftWindowsDesktop"
+    offer     = "Windows-7"
+    sku       = "win7-enterprise"
+    version   = "latest"
+  }
 
-    storage_os_disk {
-        name    = "client7disk"
-        caching = "ReadWrite"
-        create_option = "FromImage"
-    }
+  storage_os_disk {
+    name          = "client7disk"
+    caching       = "ReadWrite"
+    create_option = "FromImage"
+  }
 
-    os_profile {
-        computer_name  = "client7"
-        admin_username = "${var.username}"
-        admin_password = "${var.password}"
-    }
+  os_profile {
+    computer_name  = "client7"
+    admin_username = var.username
+    admin_password = var.password
+  }
 
-    os_profile_windows_config {
-        provision_vm_agent = true    
-    }
+  os_profile_windows_config {
+    provision_vm_agent = true
+  }
 }
+
