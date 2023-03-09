8a0c57da54fadd4c9b5bbbb433eb7405cb3059d3
xFreed0m <13978578+xFreed0m@users.noreply.github.com>
2023-03-09 14:21:13 +0200

    versions aren't managed in main.tf anymore

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 # To prevent token refresh error run this before deployment: az account get-access-token
 
 provider "azurerm" {
-  version = "=1.34.0"
+  # version = "=1.34.0"
   # subscription_id = "${var.subscription_id}"
   # client_id       = "${var.client_id}"
   # client_secret   = "${var.client_sec}"
